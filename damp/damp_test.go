6a8a6cc0ae43f0abea2e9db675cee5beeb9304ff
Alex <git@larus.se>
2025-04-09 11:49:58 +0200

    Adds first dataset

---
+++ damp/damp_test.go
@@ -0,0 +1,102 @@
+// Copyright (C) 2025 A.Svensson
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+const samplesDir string = "samples"
+
+type sample struct {
+	name      string
+	m         int
+	spi       int
+	precision float64
+}
+
+var samples = []sample{
+	{"1-bourkestreetmall", 24, 24 * 7, 0.00000000001},
+}
+
+func TestDAMPWithDatasets(t *testing.T) {
+	for _, s := range samples {
+		ts, err := readTS(filepath.Join(".", samplesDir, s.name+".in"))
+		if err != nil {
+			t.Fatal(err)
+		}
+		mp, _, _ := DAMP(ts, s.m, s.spi)
+		err = compareMP(mp, filepath.Join(samplesDir, s.name+".out"), s.precision)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+func readTS(path string) (ts TimeSeries, err error) {
+	r, err := os.Open(path)
+	if err != nil {
+		err = fmt.Errorf("Error opening input: %s\n", err)
+		return
+	}
+	ts, err = ReadTimeSeries(r)
+	if err != nil {
+		err = fmt.Errorf("Error reading input: %s\n", err)
+	}
+	defer r.Close()
+	return
+}
+
+func compareMP(mp MatrixProfile, path string, precision float64) (err error) {
+	r, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("Error opening output: %w\n", err)
+	}
+	s := bufio.NewScanner(r)
+	var f float64
+	var i int
+	for s.Scan() {
+		line := strings.TrimSpace(s.Text())
+		f, err = strconv.ParseFloat(line, 64)
+		if err != nil {
+			return
+		}
+		if !compareFloats(mp[i], f, precision) {
+			return fmt.Errorf("Expected %f, got %f\n", f, mp[i])
+		}
+		i += 1
+	}
+	return s.Err()
+}
+
+// compareFloats attempts to compare two floats, up to a certain precision.
+// Source: https://stackoverflow.com/a/76386543
+func compareFloats(a, b, epsilon float64) bool {
+	if a == b {
+		return true
+	}
+	diff := math.Abs(a - b)
+	if a == 0.0 || b == 0.0 || diff < math.SmallestNonzeroFloat64 {
+		return diff < epsilon*math.SmallestNonzeroFloat64
+	}
+	return diff/(math.Abs(a)+math.Abs(b)) < epsilon
+}
---
+++ damp/samples/README.md
@@ -0,0 +1,17 @@
+
+# Data samples
+
+This dir contains the datasets that's being used for testing discord detection
+using the DAMP algorithm.
+
+Datasets:
+
+- 1-bourkestreetmall:
+  "The City of Melbourne, Australia has developed an automated pedestrian counting
+  system to better understand pedestrian activity within the municipality,
+  such as how people use different city locations at different time of the day."
+  This was the dataset given in the reference paper.
+  Source:
+  https://timeseriesclassification.com/description.php?Dataset=MelbournePedestrian
+  Status:
+  The prototype successfully reproduced the results from the reference implementation.
